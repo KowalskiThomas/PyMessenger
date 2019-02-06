cf1119970dd8e3b7c867817b18261885df79d6fd
Thomas Kowalski <thom.kowa@gmail.com>
2019-02-06 17:03:33 +0000

    Started integrating current bot

---
+++ log.py
@@ -0,0 +1,30 @@
+import datetime
+
+class Log:
+    @staticmethod
+    def get_datetime():
+        return datetime.datetime.now().strftime("%m%d %H:%M:%S")
+
+    @staticmethod
+    def print(level, message):
+        print("[{time}] [{level}] {message}".format(
+            time = Log.get_datetime(),
+            level = level,
+            message = message
+        ))
+
+    @staticmethod
+    def warning(message):
+        Log.print("WARN", message)
+
+    @staticmethod
+    def info(message):
+        Log.print("INFO", message)
+
+    @staticmethod
+    def debug(message):
+        Log.print("DEBG", message)
+
+    @staticmethod
+    def error(message):
+        Log.print("ERRR", message)
--- server.py
+++ server.py
@@ -1,25 +1,13 @@
-from flask import Flask, Request, Response
-
-class EndpointAction(object):
-    def __init__(self):
-        # self.action = action
-        self.response = Response(status = 200, headers = dict())
-
-    def action(self):
-        pass
-
-    def __call__(self, *args):
-        self.action()
-        return self.response
-
-class HomeHandler(EndpointAction):
-    def __init__(self):
-        EndpointAction.__init__(self)
-
-    def action(self):
-        self.response.data = "coucou"
-
 from threading import Thread
+import json
+from flask import Flask, Request, Response, request
+
+from log import Log
+
+VERIFY_TOKEN = "mliqlmskdlqmskdlmqsclmkqsnfhqzopkdqlmskqsfjhqsufqlksjklqzlkdjqlksjdlqksjdliqzjpodqs"
+ACCESS_TOKEN = "EAAExE9IwmSgBABZAsN9yCHd6zkBbusjootOK9tVz68kTZBjZC5hwSMmRqDCctvWuLqltZCnX7ZAQkG8AUpXmmYQF1p0ionuZCS4pVXuuUwdNtqAhojdNu62NTqmSTyjSS7KhGUFn5d0hlxLRpeQmT94N3fDH9AOZAeyiVRYlsnvem7A4kB0vins"
+SERVER_URL = "https://158.ip-51-75-252.eu:5000"
+
 class Server(Thread):
     app = None
     bots = list()
@@ -28,13 +16,50 @@ class Server(Thread):
         Thread.__init__(self)
         self.app = Flask("MessengerBotServer")
         self.daemon = True
-        self.app.add_url_rule("/", "Home", self.on_home)
+        self.app.register_error_handler(500, self.handle_500)
+        self.app.add_url_rule("/", "Home", self.on_home, methods = ["GET", "POST"])
 
         self.bots = list()
 
+    def handle_500(self, error):
+        return json.dumps({
+            "status": 500,
+            "message": str(error)
+        })
+
     def on_home(self):
-        for b in self.bots:
-            return "ACK"
+        if request.method == 'GET':
+            Log.info("Token verification request")
+            token_sent = request.args.get("hub.verify_token")
+            return self.verify_fb_token(token_sent)
+        else:
+            data = request.get_json()
+            try:
+                print(data)
+                return "{ok}"
+            except KeyError as e:
+                print("Exception while processing request.", e)
+                return json.dumps({
+                    "status": 500,
+                    "response": "Exception raised while processing request."
+                })
+
+            return json.dumps({
+                "status": 200,
+                "response": "Message processed."
+            })
+
+    def verify_fb_token(self, token_sent):
+        #take token sent by facebook and verify it matches the verify token you sent
+        #if they match, allow the request, else return an error 
+        if token_sent == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        
+        return json.dumps({
+            "status": 403,
+            "message": "Invalid verify token."
+        })
+
 
     def register_bot(self, bot):
         self.bots.append(bot)
